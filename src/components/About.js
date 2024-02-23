017dd490ccae2e7b58b2cfa0b26e071392ea3de5
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-02-23 17:02:10 -0500

    lab completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div>
+            <h2>About</h2>
+            <p>As a software engineer, I thrive on the challenge of turning complex problems into elegant solutions. Every line of code I write is a step towards building something meaningful, whether it's crafting a user-friendly 
+              interface or optimizing algorithms for maximum efficiency</p>
+            <img src={image} alt="I made this" />
+
+        </div>;
 }
 
 export default About;
