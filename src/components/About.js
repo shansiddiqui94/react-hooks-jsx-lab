f1f69a91b7a908208512bbc5f656cc3dc4526534
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-02-24 23:34:39 -0500

    all tests passing

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,8 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>
-            <h2>About</h2>
+  return <div id="about">
+            <h2>About Me</h2>
             <p>As a software engineer, I thrive on the challenge of turning complex problems into elegant solutions. Every line of code I write is a step towards building something meaningful, whether it's crafting a user-friendly 
               interface or optimizing algorithms for maximum efficiency</p>
             <img src={image} alt="I made this" />
